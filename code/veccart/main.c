02de29640a8116a7238f22f60ea7d0a778db8f3a
Technobly <technobly@gmail.com>
2020-05-25 20:14:11 -0500

    update main.c header (new header for all files)

--- code/veccart/main.c
+++ code/veccart/main.c
@@ -1,19 +1,27 @@
-/*
- *  Copyright (C) 2015 Jeroen Domburg <jeroen at spritesmods.com>
- *
- * This library is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this library.  If not, see <http://www.gnu.org/licenses/>.
- */
+/***************************************************************************
+  Copyright (C) 2020 Brett Walach <technobly at gmail.com>
+  --------------------------------------------------------------------------
+  VEXTREME main.c
+
+  The magic starts here!
+
+  Original header follows:
+  --------------------------------------------------------------------------
+  Copyright (C) 2015 Jeroen Domburg <jeroen at spritesmods.com>
+
+  This library is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with this library.  If not, see <http://www.gnu.org/licenses/>.
+  *************************************************************************/
 
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/syscfg.h>
